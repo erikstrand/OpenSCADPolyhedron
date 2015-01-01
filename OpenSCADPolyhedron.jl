c75e29978a759c0679c924cbf86b68ce680da16b
Erik Strand <erik@erikstrand.net>
2015-01-01 14:11:34 -0800

    small update to commented documentation

--- OpenSCADPolyhedron.jl
+++ OpenSCADPolyhedron.jl
@@ -7,9 +7,9 @@
 # The key function is generatePolyhedron, which takes an initial set of points
 # and faces, a set of transformations, and a dictionary of symmetries. All the
 # transforms are applied to the initial base polyhedron, resulting in a new set
-# of points and faces. However the symmetry dictionary is used to eradicate
-# duplicate points, so that the resulting polyhedron can be described as simply
-# as possible.
+# of points and faces. The symmetry dictionary is used to eradicate duplicate
+# points, so that the resulting polyhedron can be described as simply as
+# possible.
 
 # Background
 # The polyhedron command in OpenSCAD looks like this:
